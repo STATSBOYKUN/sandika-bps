5fff4a9764060edf163fd03cbc5bd24762d7f2d4
Umar Hadi Pranoto <radenumarhp277@student.uns.ac.id>
2026-04-07 13:45:32 +0800

    bug : allowedOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	allowedDevOrigins: ["http://192.168.0.148:3001", "http://192.168.0.148"],
 	images: {
 		remotePatterns: [
 			{
